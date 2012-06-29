3ff54407c0e55e5bc8c1280e50aa826b44c321be
gpuellestorres <gpuellestorres@gmail.com>
2012-06-29 13:17:01 -0400

    Ultima version de la implementacion de MBCIF 2...

---
+++ MBCIF 2/MBCIF/Nivel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Nivel
+{
+    public List<Elemento> listaElementos;
+    public List<List<relacion>> matriz;
+
+    public Nivel()
+    {
+        listaElementos = new List<Elemento>();
+        matriz = new List<List<relacion>>();
+    }
+
+    public void agregarElemento(Elemento elementoAgregado)
+    {
+        listaElementos.Add(elementoAgregado);
+
+        List<List<relacion>> temp = new List<List<relacion>>();
+        for (int a = 0; a < listaElementos.Count; a++)
+        {
+            temp.Add(new List<relacion>());
+            for (int b = 0; b < listaElementos.Count; b++)
+            {
+                temp[a].Add(null);
+            }
+        }
+
+
+        //se copia la información que había en la matriz hasta ahora
+        for (int i = 0; i < matriz.Count; i++)
+        {
+            for (int j = 0; j < matriz[i].Count; j++)
+            {
+                temp[i][j] = matriz[i][j];
+            }
+        }
+        matriz = temp;
+    }
+
+    public void agregarRelacion(int x, int y,
+            relacion relacionEntreElementos)
+    {
+        matriz[x][y] = relacionEntreElementos;
+    }
+}
---
+++ MBCIF 2/MBCIF/regla.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class regla
+{
+    int contador = 0;
+    double valor;
+    string operador;
+    int iteracion;
+    Random random = new Random();
+
+    public regla(double Valor, string Operador, int Iteracion)
+    {
+        valor = Valor;
+        operador = Operador;
+        iteracion = Iteracion;
+
+
+    }
+
+    public regla(int Iteracion)
+    {
+        valor = 0;
+        operador = null;
+        iteracion = Iteracion;
+    }
+
+
+    public regla(double Valor, string Operador)
+    {
+        valor = Valor;
+        operador = Operador;
+        iteracion = 1;
+    }
+
+
+    public bool seCumpleRegla(double argumento)
+    {
+        contador++;
+        if (contador % iteracion == 0) ;
+        else return false;
+
+        if (operador == null) return true;
+
+        if (operador.Equals("="))
+        {
+            if (argumento == valor) return true;
+        }
+        else if (operador.Equals("<"))
+        {
+            if (argumento < valor) return true;
+        }
+        else if (operador.Equals(">"))
+        {
+            if (argumento > valor)
+            {
+                return true;
+            }
+        }
+        else if (operador.Equals("<="))
+        {
+            if (argumento <= valor) return true;
+        }
+        else if (operador.Equals(">="))
+        {
+            if (argumento >= valor) return true;
+        }
+        return false;
+    }
+
+}
